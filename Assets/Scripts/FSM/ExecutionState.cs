fd77ce8d3213b962caaece3deba3e9c083301cea
ataillat <ataillat@gaming.tech>
2023-12-13 18:02:32 +0100

    wip

---
+++ Assets/Scripts/FSM/ExecutionState.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ExecutionState
+{
+    NONE,
+    ACTIVE,
+    COMPLETED,
+    TERMINATED
+}
--- Assets/Scripts/State Machine/Enums/StateEvent.cs
+++
@@ -1,5 +0,0 @@
-public enum StateEvent
-{
-    DetectPayer,
-    LostPlayer
-}
