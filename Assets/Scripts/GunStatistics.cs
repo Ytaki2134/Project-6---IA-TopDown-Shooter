4683b7970c2755b4b8f433399fbe428891227231
Ytaki2134 <tdurand@gaming.tech>
2023-12-14 15:06:36 +0100

    Value tweaks

--- Assets/Scripts/GunStatistics.cs
+++ Assets/Scripts/GunStatistics.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 public class GunStatistics : MonoBehaviour
 {
     [SerializeField] public float BulletDamage = 10f;
-    [SerializeField] public float BulletSpeed = 2f;
-    [SerializeField] public float RotationSpeed = 4f;
+    [SerializeField] public float BulletSpeed = 3.5f;
+    [SerializeField] public float RotationSpeed = 6f;
     [SerializeField] public float BrakeRotationSpeedMod = 1f;
     [SerializeField] public GameObject BulletType;
     public bool IsPlayer;
