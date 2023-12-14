28339b93148f89403bc650ec1eb7ecee408c34cf
Ytaki2134 <tdurand@gaming.tech>
2023-12-14 14:54:32 +0100

    Shooting fully functionnal

--- Assets/Scripts/Gun.cs
+++ Assets/Scripts/Gun.cs
@@ -23,12 +23,19 @@ public class Gun : MonoBehaviour
     {
         GameObject temp = Instantiate(m_stats.BulletType, m_gunEnd.position, m_pivot.transform.rotation);
         temp.transform.rotation *= Quaternion.Euler(0, 0, 90f);
+        temp.GetComponent<Bullet>().SetGunStatsRef(m_stats);
     }
 
     private void FollowTargetPosition()
     {
         //Rotate Sprite
+
+        Debug.Log("m_targetPosition : " + m_targetPosition + " ;  transform.position : " + transform.position);
+
         var dir = (Vector3) m_targetPosition - transform.position;
+
+        Debug.Log("Direction Vector : " + dir.x + " " + dir.y);
+        Debug.Log("Atan2 : " + Mathf.Atan2(dir.y, dir.x));
 
         m_targetRotation = Quaternion.Euler(0, 0, Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg - 90f);
         m_pivot.rotation = Quaternion.Lerp(transform.rotation, m_targetRotation, Time.deltaTime * m_stats.RotationSpeed * m_stats.BrakeRotationSpeedMod);
