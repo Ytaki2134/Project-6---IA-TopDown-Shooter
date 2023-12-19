03bfc250e332e49e70e8c00615aa7a3f7fc810b0
Ytaki2134 <tdurand@gaming.tech>
2023-12-19 10:43:47 +0100

    Death SFX

--- Assets/Scripts/Tanks/Hull.cs
+++ Assets/Scripts/Tanks/Hull.cs
@@ -3,15 +3,18 @@ using UnityEngine;
 public class Hull : MonoBehaviour
 {
     [SerializeField] private Transform[] m_transformList = new Transform[2];
+    [SerializeField] private AudioClip m_DeathClip;
     private TankStatistics m_stats;
     private Animator m_animator;
     private Collider2D m_collider;
+    private AudioSource m_audioSource;
 
     void Start()
     {
         m_stats = GetComponentInParent<TankStatistics>();
         m_animator = GetComponent<Animator>();
         m_collider = GetComponent<Collider2D>();
+        m_audioSource = GetComponent<AudioSource>();
     }
 
     private void Update()
@@ -37,6 +40,10 @@ public class Hull : MonoBehaviour
 
     public void ExplodeStart()
     {
+        m_audioSource.clip = m_DeathClip;
+        m_audioSource.volume = 1f;
+        m_audioSource.Play();
+
         foreach (Transform t in m_transformList) 
         {
             Destroy(t.gameObject);
