147032275021ad93918e47ac9fdb3ea8f75a9218
Ytaki2134 <tdurand@gaming.tech>
2023-12-21 19:50:49 +0100

    small tweak

--- Assets/Scripts/Tanks/GunStatistics.cs
+++ Assets/Scripts/Tanks/GunStatistics.cs
@@ -6,7 +6,7 @@ public class GunStatistics : MonoBehaviour
     [SerializeField] public float BulletSpeed = 3.5f;
     [SerializeField] public float RotationSpeed = 6f;
     [SerializeField] public float BrakeRotationSpeedMod = 1f;
-    [SerializeField] public float ReloadTime = 2f;
+    [SerializeField] public float ReloadTime = 1f;
     [SerializeField] public GameObject BulletType;
     public bool IsPlayer;
 }
